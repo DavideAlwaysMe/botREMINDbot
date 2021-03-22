8aed0a6ed2e6bc87b1e2c082d0fce0625e6d2295
DavideAlwaysMe <davide.rizzuto@protonmail.com>
2021-03-22 09:22:22 +0100

    added cron job to delete expired reminder from database

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 #clono il repository
 rm -r botREMINDbot
 git clone https://github.com/DavideAlwaysMe/botREMINDbot.git
-cront -f &
+cron -f &
 echo $1 $2
 python3 /botREMINDbot/cron_update.py $1 $2
 python3 /botREMINDbot/main.py $1 $2
